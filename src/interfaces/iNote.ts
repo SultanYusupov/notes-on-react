d3b6b124bd166db9d39132c8fdcbbef9b70beb91
Sultan <s.yusupov.prog@solo-it.ru>
2025-01-30 14:23:59 +0400

    init

---
+++ src/interfaces/iNote.ts
@@ -0,0 +1,6 @@
+export interface iNote {
+    id: string,
+    title: string,
+    text: string,
+    dateCreate?: string
+}
---
+++ src/services/NoteService.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+import {iNote} from "../interfaces/iNote.ts";
+
+class NoteService {
+    private URL = 'http://localhost:3000/note-list';
+    noteListLength: number = 0;
+    async getNoteList() {
+        const {data} = await axios.get<iNote[]>(this.URL);
+        return data;
+    }
+
+    async getNoteItem(id: string) {
+        const {data} = await axios.get<iNote>(this.URL+'/'+id);
+        return data;
+    }
+
+    async editNote(id: string, content: object) {
+        const {data} = await axios.patch<iNote>(this.URL+'/'+id, content);
+        return data;
+    }
+
+    async createNote(content: iNote) {
+        const {data} = await axios.post<iNote>(this.URL, content);
+        return data;
+    }
+
+    async deleteNote(id: string) {
+        const {data} = await axios.delete<iNote>(this.URL+'/'+id);
+        return data;
+    }
+
+    get listLength() {
+        return this.noteListLength;
+    }
+    set listLength(length: number) {
+        this.noteListLength = length;
+    }
+}
+
+export const noteService = new NoteService();
