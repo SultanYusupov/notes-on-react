014792edb36c28bbac949087999425fda44ffd12
Sultan <s.yusupov.prog@solo-it.ru>
2025-01-31 20:53:24 +0400

    preparing store and slice

---
+++ src/state/notesSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from './store'
+import {iNote} from "../interfaces/iNote.ts";
+
+export interface NotesState {
+    noteList: iNote[]
+}
+
+const initialState: NotesState = {
+    noteList: []
+}
+
+export const notesSlice = createSlice({
+    name: 'notesState',
+    // `createSlice` will infer the state type from the `initialState` argument
+    initialState,
+    reducers: {
+        add: (state, action: PayloadAction<iNote>) => {
+            return {...state, noteList: [...state.noteList, action.payload]}
+        },
+        delete: (state, action: PayloadAction<number>) => {
+            return {...state, noteList: state.noteList.filter(n => n.id !== action.payload)}
+        },
+        // Use the PayloadAction type to declare the contents of `action.payload`
+        edit: (state, action: PayloadAction<iNote>) => {
+            return {...state, noteItem: state.noteList.map(n => n.id == action.payload.id ? action.payload : n)};
+        }
+    }
+})
+
+export const { add, delete, edit } = notesSlice.actions
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectCount = (state: RootState) => state.notes // state.counter.value
+
+export default notesSlice.reducer
