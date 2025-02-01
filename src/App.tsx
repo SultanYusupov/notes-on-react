c2d67809f91a57e570899cad1de8968da3ae83e6
Sultan <s.yusupov.prog@solo-it.ru>
2025-02-01 17:41:49 +0400

    eliminated double useEffect call

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,36 @@ import NotePreview from "./components/NotePreview.tsx";
 import {RootState} from "./state/store.ts";
 import {useAppDispatch, useAppSelector} from "./hooks/redux-hooks.ts";
 import { add } from './state/notesSlice'
-import {useEffect} from "react";
+import {useEffect, useRef} from "react";
 import {noteService} from "./services/note.service.ts";
 import {iNote} from "./interfaces/iNote.ts";
 
 function App() {
   const dispatch = useAppDispatch();
-  const notes = useAppSelector((state: RootState) => state.notes.noteList)
-  const note = {
-    id: 0,
-    title: 'Static Title',
-    text: 'Static Text',
-    dateCreate: '30 January 2025'
-  }
+  const notes = useAppSelector((state: RootState) => state.notes.noteList);
+  const isEffectRun = useRef(false); // Флаг для отслеживания выполнения эффекта
   useEffect(() => {
+    if (isEffectRun.current) return; // Если эффект уже выполнен, выходим
+    isEffectRun.current = true; // Устанавливаем флаг
     const asyncNoteListFunc = async () => {
       return await noteService.getNoteList();
-
     };
     asyncNoteListFunc().then((result: iNote[]) => {
-      result.map(r => {
+      result.forEach(r => {
         dispatch(add(r));
-        console.log(notes)
       });
+
     });
-  }, [])
+  }, [dispatch])
 
   return (
     <>
-    <NotePreview note={note} />
+      {notes.length ? (notes.map((note:iNote) => {
+        return <NotePreview note={note} key={note.id}/>
+      })) : (
+          <h4>У вас пока нет заметок. Давайте создадим новую!</h4>
+      )}
+
     </>
   )
 }
