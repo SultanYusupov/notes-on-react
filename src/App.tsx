1802e79f27759f1eb4d93051121e19f346278c2d
Sultan <s.yusupov.prog@solo-it.ru>
2025-02-01 16:09:50 +0400

    using redux, but got error Failed to resolve import "react-redux" from "src/hooks/redux-hooks.ts". Does the file exist?

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import './App.css'
 import NotePreview from "./components/NotePreview.tsx";
+import {RootState} from "./state/store.ts";
+import {useAppDispatch, useAppSelector} from "./hooks/redux-hooks.ts";
+import { add } from './state/notesSlice'
+import {useEffect} from "react";
+import {noteService} from "./services/note.service.ts";
+import {iNote} from "./interfaces/iNote.ts";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const notes = useAppSelector((state: RootState) => state.notes.noteList)
   const note = {
     id: 0,
     title: 'Static Title',
     text: 'Static Text',
     dateCreate: '30 January 2025'
   }
+  useEffect(() => {
+    const asyncNoteListFunc = async () => {
+      return await noteService.getNoteList();
+
+    };
+    asyncNoteListFunc().then((result: iNote[]) => {
+      result.map(r => {
+        dispatch(add(r));
+        console.log(notes)
+      });
+    });
+  }, [])
 
   return (
     <>
