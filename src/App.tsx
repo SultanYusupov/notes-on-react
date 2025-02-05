5189112848f551700fbe9b756a5c709b2f81f1f1
Sultan <s.yusupov.prog@solo-it.ru>
2025-02-05 14:20:52 +0400

    Deleted default styles. Changed file extension css -> scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.scss'
 import {Route, Routes} from "react-router";
 import NoteList from "./components/NoteList.tsx";
 import {NoteContent} from "./components/NoteContent.tsx";
