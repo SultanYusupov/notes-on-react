490ed72753bf4407e89a057a160b901bdf823bba
Sultan <s.yusupov.prog@solo-it.ru>
2025-02-09 12:12:04 +0400

    additionally specified data types

--- src/state/noteSelector.ts
+++ src/state/noteSelector.ts
@@ -1,11 +1,13 @@
 import { createSelector } from '@reduxjs/toolkit';
-import type { RootState } from './store.ts'; // импорт типа корневого состояния
+import type { RootState } from './store.ts';
+import {iNote} from "../interfaces/iNote.ts"; // импорт типа корневого состояния
 
 // Предположим, что у вас есть слайс с именем 'items' и в нём массив 'items'
-const selectNotes = (state: RootState) => state.notes.noteList;
+const selectedNotes = (state: RootState) => state.notes.noteList;
 
 // Селектор для получения элемента по id
 export const selectItemById = createSelector(
-    [selectNotes, (_state: RootState, noteId: number) => noteId],
-    (noteList, noteId) => noteList.find(note => note.id === noteId)
+    [selectedNotes, (_state: RootState, noteId: number) => noteId],
+    (noteList:iNote[], noteId) => <iNote>noteList.find(note => note.id == noteId)
 );
+export const selectArrayLength = createSelector(selectedNotes, (selectedNotes) => selectedNotes.length)
