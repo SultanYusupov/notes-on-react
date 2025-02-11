30c77a2d04f2852f115833d163ac955f263861e6
Sultan <s.yusupov.prog@solo-it.ru>
2025-02-11 13:29:56 +0400

    new initial values, checking data changes and displaying "check" button

--- src/components/NoteContent.tsx
+++ src/components/NoteContent.tsx
@@ -11,11 +11,13 @@ export function NoteContent() {
     const {id} = useParams<string>();
     const noteId: number = Number(id);
     const [showEditIcon, setEditIcon] = useState(false);
-    const [note, setNote] = useState<iNote>({id: noteId, title: '', text: '', dateCreate: ''});
-
     const existingNote = useAppSelector(state => {
         return selectItemById(state, noteId)
     });
+    const initialTitle = existingNote?.title ?? '';
+    const initialText = existingNote?.text ?? '';
+    // id нет, это чтобы сервер сгенерировал его для новой записи
+    const [note, setNote] = useState<iNote>({title: initialTitle, text: initialText, dateCreate: ''});
     const asyncNoteItemFunc = async (id:string) => {
         return await noteService.getNoteItem(id);
     }
@@ -30,25 +32,45 @@ export function NoteContent() {
         }
 
     }, [existingNote, id]);
+    
+    useEffect(() => {
+        // галочка появляется только когда мы реально что-то изменили в тексте
+        // если что-то написали, затем это стёрли, то изменений по факту нет
+        setEditIcon(note.title !== initialTitle || note.text !== initialText)
+    }, [note.title, initialTitle, note.text, initialText])
+
+    function save() {
+        // hide check icon
+        // check if state not changed
+        // if state changed, update via use State
+        // correct date
+        // send new data to server
+        setEditIcon(false);
+        if (Number.isNaN(noteId)) {
+            noteService.createNote(note).then(res => console.log(res))
+        }
+        else {
+            noteService.editNote(id!, note).then(res => console.log(res))
+        }
+    }
 
     return (
         <div style={{margin: '0 auto', width: '600px',}}>
             <Header style={{margin: '1rem 0', with: '100%', padding: '0.1rem'}} displayBackButton={true}>
-                {showEditIcon && <i className="bi bi-check-lg" role={"button"}></i>}
+                {showEditIcon && <i className="bi bi-check-lg" role={"button"} onClick={() => save()}></i>}
                 <i className="bi bi-trash3" style={{paddingLeft: '1rem'}} role={"button"}></i>
             </Header>
             <Card style={{width: '100%'}}>
                 <Card.Body>
                     <Card.Title className={'border-bottom pb-2'}><Form.Control className={'border-0'} type={"text"}
                                                                                value={note.title}
-                                                                               onFocus={() => setEditIcon(true)}
                                                                                onChange={(e) => setNote({
                                                                                    ...note,
                                                                                    title: e.target.value
                                                                                })}/></Card.Title>
                     <Card.Text>
                         <Form.Control className={'border-0'} as="textarea" rows={3} value={note.text}
-                                      onFocus={() => setEditIcon(true)} onChange={(e) => setNote({
+                                      onChange={(e) => setNote({
                             ...note,
                             text: e.target.value
                         })}/>
