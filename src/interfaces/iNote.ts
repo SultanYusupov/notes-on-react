9c588c24362619f15a56ea47d27e9ae512a1d466
Sultan <s.yusupov.prog@solo-it.ru>
2025-02-12 21:40:09 +0400

    created new interface

--- src/interfaces/iNote.ts
+++ src/interfaces/iNote.ts
@@ -1,6 +1,10 @@
 export interface iNote {
-    id?: number,
+    id?: number,// TODO убрать вопросительный знак
     title: string,
     text: string,
     dateCreate: string
 }
+
+// когда отправляем запрос на создание новой заметки, у него нет id
+// это как раз делает интерфейс, похожий на iNote, но без поля id
+export interface iNoteData extends Omit<iNote, 'id'>{}
