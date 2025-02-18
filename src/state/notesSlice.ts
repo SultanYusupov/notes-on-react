5b1006c3009380d6f67fda1eb6cfda476472b319
Sultan <s.yusupov.prog@solo-it.ru>
2025-02-18 20:07:13 +0400

    Deleted import

--- src/state/notesSlice.ts
+++ src/state/notesSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import type { RootState } from './store'
 import {iNote} from "../interfaces/iNote.ts";
 
 export interface NotesState {
