e5cef19b098dccfb691fa21d5f6af313654b00a2
Sultan <s.yusupov.prog@solo-it.ru>
2025-03-12 00:30:17 +0400

    changes in api.ts

--- src/state/api/api.ts
+++ src/state/api/api.ts
@@ -1,10 +1,10 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
-const API_URL = 'http://localhost:3001/note-list';
+const API_URL = 'http://localhost:5000/api';
 export const api = createApi({
     reducerPath: 'api',
-    tagTypes: ['Notes'],
-    baseQuery: fetchBaseQuery({ baseUrl: API_URL}), // TODO здесь есть headers
+    tagTypes: ['Notes', 'Auth'],
+    baseQuery: fetchBaseQuery({ baseUrl: API_URL}), // TODO здесь есть headers: {Authorization: `Bearer ${token}`}
     endpoints: () => ({}),
 })
 
