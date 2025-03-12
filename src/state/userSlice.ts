9e5475b353dda479ce7c468d4042ec4a43b007da
Sultan <s.yusupov.prog@solo-it.ru>
2025-03-12 16:11:30 +0400

    new user slice

---
+++ src/state/userSlice.ts
@@ -0,0 +1,31 @@
+import {iUser} from "../interfaces/iUser.ts";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+interface UserState{
+    user: iUser,
+    isAuth: boolean,
+    isLoading: boolean
+}
+const initialState: UserState = {
+    user: {} as iUser,
+    isAuth: false,
+    isLoading: false
+}
+export const userSlice = createSlice({
+    name: 'userState',
+    initialState,
+    reducers: {
+        setAuth: (state:UserState, action:PayloadAction<boolean>) => {
+            return {...state, isAuth: action.payload}
+        },
+        setUser: (state:UserState, action:PayloadAction<iUser>) => {
+            return {...state, user: action.payload}
+        },
+        setLoading: (state:UserState, action:PayloadAction<boolean>) => {
+            return {...state, isLoading: action.payload}
+        },
+        logout: () => initialState
+    }
+});
+export const {setLoading, setAuth, setUser, logout} = userSlice.actions;
+export default userSlice.reducer;
