8c7fd9d97b6ef79bcda142c60f646b660ab205e0
Sultan <s.yusupov.prog@solo-it.ru>
2025-03-13 00:27:17 +0400

    header Bearer Token

--- src/state/api/api.ts
+++ src/state/api/api.ts
@@ -1,9 +1,10 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 const API_URL = 'http://localhost:5000/api';
+const token = localStorage.getItem('token');
 export const api = createApi({
     reducerPath: 'api',
     tagTypes: ['Notes', 'Auth'],
-    baseQuery: fetchBaseQuery({ baseUrl: API_URL}), // TODO здесь есть headers: {Authorization: `Bearer ${token}`}
+    baseQuery: fetchBaseQuery({ baseUrl: API_URL, headers: {Authorization: `Bearer ${token}`}}),
     endpoints: () => ({}),
 })
